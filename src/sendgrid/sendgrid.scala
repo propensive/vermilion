13a83f98028aa76a987e1a0db94f3bc5451602b0
Jon Pretty <jon.pretty@propensive.com>
2021-06-30 18:36:18 +0200

    Switch to Scala 3 syntax

--- src/sendgrid/sendgrid.scala
+++ src/sendgrid/sendgrid.scala
@@ -2,52 +2,44 @@ package vermilion
 
 import com.sendgrid._, helpers.mail._, objects._
 
-import rudiments._
+import rudiments.*
 
-import scala.util._
+import scala.util.*
 
 import java.io.IOException
-import java.util.HashMap
-import java.util.Map
 
 case class SendException() extends RuntimeException("the email could not be sent")
 case class SendAddressException() extends RuntimeException("the email could not be sent")
 
-case class MailtoUri(email: String) {
+case class MailtoUri(email: String):
   override def toString = s"mailto:$email"
-}
 
-case class HtmlEmail(html: String, inlines: List[Annex[Attachable]], attachments: List[Annex[Attachable]])
+case class HtmlEmail(html: String, inlines: List[Annex[Attachable]],
+                         attachments: List[Annex[Attachable]])
 
 
-case class Contact(email: String, name: Option[String] = None) {
+case class Contact(email: String, name: Option[String] = None):
   override def toString = if (name == None) email else s""""${name.get}" <${email}>"""
-}
 
-trait Mailable[T] {
+trait Mailable[T]:
   def bodyText(t: T): String
   def bodyHtml(t: T): Option[(String, Seq[Annex[Attachable]])]
   def attachments(t: T): Seq[Annex[Attachable]]
-}
 
-object Mailable {
-  implicit val stringMailable: Mailable[String] = new Mailable[String] {
+object Mailable:
+  implicit val stringMailable: Mailable[String] = new Mailable[String]:
     def bodyText(t: String) = t
     def bodyHtml(t: String) = None
     def attachments(t: String): Seq[Annex[Attachable]] = Nil
-  }
 
   implicit def htmlMailable(implicit conv: HtmlToPlainTextConverter): Mailable[HtmlEmail] =
-    new Mailable[HtmlEmail] {
+    new Mailable[HtmlEmail]:
       def bodyText(t: HtmlEmail) = conv.convert(t.html)
       def bodyHtml(t: HtmlEmail) = Some((t.html, t.inlines))
       def attachments(t: HtmlEmail) = t.attachments
-    }
 
-}
-
-object EmailMessage {
-  implicit val emailMessageSendable: Sendable[EmailMessage] = new Sendable[EmailMessage] {
+object EmailMessage:
+  implicit val emailMessageSendable: Sendable[EmailMessage] = new Sendable[EmailMessage]:
     def to(env: EmailMessage): Seq[Contact] = env.to
     def cc(env: EmailMessage): Seq[Contact] = env.cc
     def bcc(env: EmailMessage): Seq[Contact] = env.bcc
@@ -56,13 +48,13 @@ object EmailMessage {
     def bodyText(env: EmailMessage): String = env.mailable(_.bodyText)
     def bodyHtml(env: EmailMessage): Option[(String, Seq[Annex[Attachable]])] = env.mailable(_.bodyHtml)
     def attachments(env: EmailMessage): Seq[Annex[Attachable]] = env.mailable(_.attachments)
-  }
-}
 
-case class EmailMessage(from: Contact, to: Seq[Contact], cc: Seq[Contact], bcc: Seq[Contact], subject: String, mailable: Annex[Mailable], attachments: Seq[Annex[Attachable]]*)
+case class EmailMessage(from: Contact, to: Seq[Contact], cc: Seq[Contact], bcc: Seq[Contact],
+                            subject: String, mailable: Annex[Mailable],
+                            attachments: Seq[Annex[Attachable]]*)
 
-object Sendable {
-  case class Capability[T: Sendable](msg: T) {
+object Sendable:
+  case class Capability[T: Sendable](msg: T):
     def send()(implicit sendmailBackend: SendmailBackend): Try[String] =
       sendmailBackend.sendmail(
         to = implicitly[Sendable[T]].to(msg).map(_.toString),
@@ -74,10 +66,8 @@ object Sendable {
         bodyHtml = implicitly[Sendable[T]].bodyHtml(msg),
         attachments = implicitly[Sendable[T]].attachments(msg)
       )
-  }
-}
 
-trait Sendable[T] {
+trait Sendable[T]:
   def to(t: T): Seq[Contact]
   def cc(t: T): Seq[Contact]
   def bcc(t: T): Seq[Contact]
@@ -86,56 +76,43 @@ trait Sendable[T] {
   def bodyText(t: T): String
   def bodyHtml(t: T): Option[(String, Seq[Annex[Attachable]])]
   def attachments(t: T): Seq[Annex[Attachable]]
-}
 
-case class Envelope(
-  subject: String,
-  from: Contact,
-  to: Seq[Contact],
-  cc: Seq[Contact] = Nil,
-  bcc: Seq[Contact] = Nil) {
-
+case class Envelope(subject: String, from: Contact, to: Seq[Contact], cc: Seq[Contact] = Nil,
+                        bcc: Seq[Contact] = Nil):
   def insert(mailable: Annex[Mailable], attachments: Annex[Attachable]*): EmailMessage =
     EmailMessage(from, to, cc, bcc, subject, mailable, attachments)
-}
 
-object sendgridBackend extends SendmailBackend {
+object sendgridBackend extends SendmailBackend:
 
-  def extract(email: String): (String, String) = email.trim.split("<", 2) match {
+  def extract(email: String): (String, String) = email.trim.split("<", 2) match
     case Array(name, e) => (name.trim, e.trim.dropRight(1))
     case Array(e) => ("", e)
-  }
 
-  def base64(data: Array[Byte]): String = new String(java.util.Base64.getEncoder().encode(data))
+  def base64(data: Array[Byte]): String = String(java.util.Base64.getEncoder().encode(data))
 
-  def sendmail(to: Seq[String],
-               from: String,
-               subject: String,
-               cc: Seq[String],
-               bcc: Seq[String],
-               bodyText: String,
-               bodyHtml: Option[(String, Seq[Annex[Attachable]])],
-               attachments: Seq[Annex[Attachable]]): Try[String] = Try {
+  def sendmail(to: Seq[String], from: String, subject: String, cc: Seq[String], bcc: Seq[String],
+                   bodyText: String, bodyHtml: Option[(String, Seq[Annex[Attachable]])],
+                   attachments: Seq[Annex[Attachable]]): Try[String] = Try {
 
     val mail = new Mail()
 
     val fromEmail = new Email()
     val fromContact = extract(from)
-    fromEmail.setName(fromContact._1.replaceAll("\"", ""))
-    fromEmail.setEmail(fromContact._2)
+    fromEmail.setName(fromContact.at[0].replaceAll("\"", ""))
+    fromEmail.setEmail(fromContact.at[1])
     mail.setFrom(fromEmail)
 
     mail.setSubject(subject)
 
     val personalization = new Personalization()
-    to.map(extract).foreach { case (toName, toEmail) =>
+    to.map(extract).foreach { (toName, toEmail) =>
       val to = new Email()
       to.setName(toName)
       to.setEmail(toEmail)
       personalization.addTo(to)
     }
 
-    cc.map(extract).foreach { case (ccName, ccEmail) =>
+    cc.map(extract).foreach { (ccName, ccEmail) =>
       val cc = new Email()
       cc.setName(ccName)
       cc.setEmail(ccEmail)
@@ -179,7 +156,7 @@ object sendgridBackend extends SendmailBackend {
     val sg = new SendGrid("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")
 
     val request = new com.sendgrid.Request()
-    try {
+    try
       request.setMethod(com.sendgrid.Method.POST)
       request.setEndpoint("mail/send")
       request.setBody(mail.build())
@@ -188,23 +165,19 @@ object sendgridBackend extends SendmailBackend {
       println(response.getBody())
       println(response.getHeaders())
       response.getStatusCode().toString
-    } catch {
-      case ex: IOException => throw ex
-    }
+    catch case ex: IOException => throw ex
   }
 }
 
-object htmlToPlainTextConverters {
+object htmlToPlainTextConverters:
 
-  object plainText {
-    implicit val htmlToPlainTextConverter: HtmlToPlainTextConverter = new HtmlToPlainTextConverter {
+  object plainText:
+    implicit val htmlToPlainTextConverter: HtmlToPlainTextConverter = new HtmlToPlainTextConverter:
       def convert(html: String): String = html.replaceAll("<[^>]>", "")
-    }
-  }
 
-  object elinks {
-    implicit val htmlToPlainTextConverter: HtmlToPlainTextConverter = new HtmlToPlainTextConverter {
-      def convert(html: String): String = {
+  object elinks:
+    implicit val htmlToPlainTextConverter: HtmlToPlainTextConverter = new HtmlToPlainTextConverter:
+      def convert(html: String): String =
         val file = new java.io.File(s"/tmp/${java.util.UUID.randomUUID().toString}.html")
         val fw = new java.io.FileWriter(file)
         fw.write(html)
@@ -213,13 +186,10 @@ object htmlToPlainTextConverters {
         val output = sh"elinks -dump -dump-width 76 '${file.getAbsolutePath}'".exec[String]
         file.delete()
         output
-      }
-    }
-  }
 
-  object links {
-    implicit val htmlToPlainTextConverter: HtmlToPlainTextConverter = new HtmlToPlainTextConverter {
-      def convert(html: String): String = {
+  object links:
+    implicit val htmlToPlainTextConverter: HtmlToPlainTextConverter = new HtmlToPlainTextConverter:
+      def convert(html: String): String =
         val file = new java.io.File(s"/tmp/${java.util.UUID.randomUUID().toString}.html")
         val fw = new java.io.FileWriter(file)
         fw.write(html)
@@ -228,13 +198,10 @@ object htmlToPlainTextConverters {
         val output = sh"links -dump 'file://${file.getAbsolutePath}'".exec[String]
         file.delete()
         output
-      }
-    }
-  }
 
-  object lynx {
-    implicit val htmlToPlainTextConverter: HtmlToPlainTextConverter = new HtmlToPlainTextConverter {
-      def convert(html: String): String = {
+  object lynx:
+    implicit val htmlToPlainTextConverter: HtmlToPlainTextConverter = new HtmlToPlainTextConverter
+      def convert(html: String): String =
         val file = new java.io.File(s"/tmp/${java.util.UUID.randomUUID().toString}.html")
         val fw = new java.io.FileWriter(file)
         fw.write(html)
@@ -243,12 +210,6 @@ object htmlToPlainTextConverters {
         val output = sh"lynx -dump -width=76 'file://${file.getAbsolutePath}'".exec[String]
         file.delete()
         output
-      }
-    }
-  }
 
-}
-
-trait HtmlToPlainTextConverter {
+trait HtmlToPlainTextConverter:
   def convert(html: String): String
-}
